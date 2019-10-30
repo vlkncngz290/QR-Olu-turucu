2937255b6e42147019f2bc8b68797f61ff3c56f7
volkan cengiz <volkancengiz@outlook.com>
2019-10-30 10:26:34 +0300

    Add files via upload

---
+++ Bitekser QR Oluşturucu/Bitekser QR Oluşturucu/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Bitekser_QR_Oluşturucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnQROlustur = new System.Windows.Forms.Button();
+            this.exlAktar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(12, 12);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(212, 20);
+            this.txt1.TabIndex = 0;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(290, 12);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(212, 20);
+            this.txt2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 181);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(563, 12);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(212, 20);
+            this.txt3.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(290, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(212, 181);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(563, 38);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(212, 181);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnQROlustur
+            // 
+            this.btnQROlustur.Location = new System.Drawing.Point(348, 226);
+            this.btnQROlustur.Name = "btnQROlustur";
+            this.btnQROlustur.Size = new System.Drawing.Size(75, 23);
+            this.btnQROlustur.TabIndex = 6;
+            this.btnQROlustur.Text = "QR Oluştur";
+            this.btnQROlustur.UseVisualStyleBackColor = true;
+            this.btnQROlustur.Click += new System.EventHandler(this.btnQROlustur_Click);
+            // 
+            // exlAktar
+            // 
+            this.exlAktar.Location = new System.Drawing.Point(563, 225);
+            this.exlAktar.Name = "exlAktar";
+            this.exlAktar.Size = new System.Drawing.Size(101, 23);
+            this.exlAktar.TabIndex = 7;
+            this.exlAktar.Text = "Excel\'e Aktar";
+            this.exlAktar.UseVisualStyleBackColor = true;
+            this.exlAktar.Click += new System.EventHandler(this.exlAktar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 273);
+            this.Controls.Add(this.exlAktar);
+            this.Controls.Add(this.btnQROlustur);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnQROlustur;
+        private System.Windows.Forms.Button exlAktar;
+    }
+}
+
---
+++ Bitekser QR Oluşturucu/Bitekser QR Oluşturucu/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Bitekser_QR_Oluşturucu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQROlustur_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw qrCode1 = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            Zen.Barcode.CodeQrBarcodeDraw qrCode2 = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            Zen.Barcode.CodeQrBarcodeDraw qrCode3 = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            pictureBox1.Image = qrCode1.Draw("isim:"+txt1.Text+"soyisim"+txt2.Text+"firma:"+txt3.Text,1000);
+            pictureBox2.Image = qrCode2.Draw(txt2.Text, 100);
+            pictureBox3.Image = qrCode3.Draw(txt3.Text, 100);
+        }
+
+        private void exlAktar_Click(object sender, EventArgs e)
+        {
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = true;
+            object Missing = Type.Missing;
+            Workbook workbook = excel.Workbooks.Add(Missing);
+            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+            //sheet1.Cells[1, 3] = pictureBox1.Image;
+        }
+
+        
+
+    }
+}
